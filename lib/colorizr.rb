e9ab4fab19a7349ac9ddeef8c32c98314aebf26a
mikeyjcat <mike@hewitson.co.za>
2016-02-03 05:55:34 +0200

    Corrected small style issues in ruby code and improved readme file

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,17 +1,17 @@
 # Overrides the String class to add methods to colorize the string
 class String
   COLOR_CODES = { red: 31, green: 32, yellow: 33, blue: 34, pink: 35,
-                  light_blue: 36, white: 97, light_grey: 37, black: 30 }
+                  light_blue: 36, white: 97, light_grey: 37, black: 30 }.freeze
 
   def self.colors
-    COLOR_CODES.each_key.entries
+    COLOR_CODES.keys
   end
 
   # Method to create the color methods, based of the list of colors and
   # color codes
   def self.create_colors
     COLOR_CODES.each do |color, code|
-      String.send(:define_method, "#{color}") do
+      send(:define_method, color) do
         "\e[#{code}m#{self}\e[0m"
       end
     end
